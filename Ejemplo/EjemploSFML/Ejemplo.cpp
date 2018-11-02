440876ff5bd0e242a1b53aa965463faffdf77e6f
Degarrido <de.garrido95@gmail.com>
2018-11-02 11:53:42 -0300

    Added Player Movement

--- Ejemplo/EjemploSFML/Ejemplo.cpp
+++ Ejemplo/EjemploSFML/Ejemplo.cpp
@@ -4,10 +4,13 @@ int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 	sf::RectangleShape player(sf::Vector2f(120.f,50.f));
+	sf::RectangleShape player2(sf::Vector2f(120.f, 50.f));
 	sf::CircleShape ball(20.f);
 	ball.setFillColor(sf::Color::Green);
 	player.setFillColor(sf::Color::Blue);
 	player.setSize(sf::Vector2f(30.f, 150.f));
+	player2.setSize(sf::Vector2f(30.f, 150.f));
+	player2.setPosition(770, 550);
 	ball.setPosition(250, 250);
 
 	while (window.isOpen())
@@ -26,8 +29,17 @@ int main()
 		{
 			player.move(0.f, 0.2);
 		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			player2.move(0.f, -0.2);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			player2.move(0.f, 0.2);
+		}
 		window.clear();
 		window.draw(player);
+		window.draw(player2);
 		window.draw(ball);
 		window.display();
 	}
